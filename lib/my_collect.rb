8d64e9f639acef1549a5426580cf13d5dcce1916
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-09-19 22:22:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
-
+def my_collect(arr)
+  if block_given? == lang
+  i = 0
+  
+  while i < arr.length
+    yield arr[i].upcase
+    i = i + 1
+  end
+  
+  arr
+  else
+    i = 0
+  
+    while i < arr.length
+      yield arr[i].split(" ").first
+      i = i + 1
+    end
+  arr
+  end
+end
 
