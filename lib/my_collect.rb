f8022b14514d23dcdebbe79a019d9035f4f94e8d
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-09-19 22:25:01 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,4 @@
 def my_collect(arr)
-  if block_given? == lang
   i = 0
   
   while i < arr.length
@@ -7,14 +6,6 @@ def my_collect(arr)
     i = i + 1
   end
   
-  arr
-  else
-    i = 0
-  
-    while i < arr.length
-      yield arr[i].split(" ").first
-      i = i + 1
-    end
   arr
   end
 end
